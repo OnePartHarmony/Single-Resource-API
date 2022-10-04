30993396a78cc85f8f1fe9d8e143f61c544df8a1
OnePartHarmony <bakingharmony@gmail.com>
2022-10-04 17:06:03 -0400

    moved db connections to connection.js

--- server.js
+++ server.js
@@ -6,18 +6,7 @@ const path = require("path")
 
 const Cake = require("./models/cakes")
 
-const DATABASE_URL = process.env.DATABASE_URL
-const CONFIG = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}
 
-mongoose.connect(DATABASE_URL, CONFIG)
-
-mongoose.connection
-    .on("open", () => console.log("Connected to Mongoose"))
-    .on("close", () => console.log("Disconnected from Mongoose"))
-    .on("error", (error) => console.log(error))
 
 const app = express()
 
