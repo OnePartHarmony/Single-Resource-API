1ffd25cc49ff1f2f3575640877f5648b2982ca54
OnePartHarmony <bakingharmony@gmail.com>
2022-10-05 19:36:01 -0400

    new route creates comment on bread or cake based on id

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,37 +3,38 @@ const express = require("express")
 const Cake = require("../models/cakes")
 const Bread = require("../models/bread")
 
-
+//////create router///////
+const router = express.Router()
 
 /////Routes//////////
 
-//post comment route - can only be done when logged in/////
-// router.post("/:cakeOrBreadId", (req,res) => {
-//     const id = req.params.cakeOrBreadId
-//     if (req.session.loggedIn) {
-//         req.body.author = req.session.userId
-//     } else {
-//         res.sendStatus(401)
-//         return
-//     }
-//     Cake.findById(id)
-//         .then(cake => {
-//             if (cake == null) {
-//                 Bread.findById(id)
-//                     .then(bread => {
-//                         bread.comments.push(req.body)
-//                         bread.save()
-//                         res.status(200).json({bread: bread})
-//                     })
-//                     .catch(error => console.error(error))
-//             } else {
-//                 cake.comments.push(req.body)
-//                 cake.save()
-//                 res.status(200).json({cake: cake})
-//             }
-//         })
-//         .catch(error => console.error(error))
-// })
+////post comment route - can only be done when logged in/////
+router.post("/:cakeOrBreadId", (req,res) => {
+    const id = req.params.cakeOrBreadId
+    if (req.session.loggedIn) {
+        req.body.author = req.session.userId
+    } else {
+        res.sendStatus(401)
+        return
+    }
+    Cake.findById(id)
+        .then(cake => {
+            if (cake == null) {
+                Bread.findById(id)
+                    .then(bread => {
+                        bread.comments.push(req.body)
+                        bread.save()
+                        res.status(200).json({bread: bread})
+                    })
+                    .catch(error => console.error(error))
+            } else {
+                cake.comments.push(req.body)
+                cake.save()
+                res.status(200).json({cake: cake})
+            }
+        })
+        .catch(error => console.error(error))
+})
 
 
 
