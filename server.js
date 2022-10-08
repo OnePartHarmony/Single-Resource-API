4e79da1079c93be196268417f7c2f71e3d0ae470
OnePartHarmony <bakingharmony@gmail.com>
2022-10-08 10:34:09 -0400

    created error view and error route in server.js, plus a catch all route for bad pathing

--- server.js
+++ server.js
@@ -31,6 +31,23 @@ app.use("/user", UserRouter)
 app.use("/comments", CommentRouter)
 
 
+//////error route/////////
+app.get("/error", (req,res) => {
+    // const username = req.session.username
+    // const loggedIn = req.session.loggedIn
+    // const userId = req.session.userId
+    const {username, loggedIn, userId} = req.session
+    const error = req.query.error || "This page does not exist"
+    res.render("error.liquid", {error, username, loggedIn, userId})
+})
+
+
+//////catch-all route for non-existent paths/////
+app.all("*", (req,res) => {
+    res.redirect("/error")
+})
+
+
 /////Server Listener///////
 const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Now listening on port ${PORT}`))
